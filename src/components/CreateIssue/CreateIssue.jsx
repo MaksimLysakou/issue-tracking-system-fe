fa5ba27d975f32d039f71c068558cfb2b789a386
SashaKuzmenko <blok_95_sa@mail.ru>
2018-12-06 16:02:48 +0300

    add create issue page

---
+++ src/components/CreateIssue/CreateIssue.jsx
@@ -0,0 +1,67 @@
+import React, {PureComponent} from 'react';
+import './style.css';
+
+class CreateIssue extends PureComponent{
+    
+    render () {
+        return ( 
+            <div className="maincontainer">
+                <div className="head">
+                    <h1> Страница создания задачи </h1> 
+                </div>
+                <div className="mainelements">
+                    <div className="inputgroup">
+                        <label> Название задачи: </label>
+                        <input type="text" name="name" />
+                    </div>
+
+                    <div className="inputgroup">                
+                        <label> Описание: </label>
+                        <textarea type="text" name="message"/>
+                    </div>
+
+                    <div className="inputgroup">
+                        <label> Создатель задачи: </label>
+                        <label> Admin </label>
+                    </div> 
+
+                    <div className="inputgroup"> 
+                        <label> Выберите исполнителя:</label>
+                        <select >
+                            <option value="Команда 1">Команда 1</option>
+                            <option value="Команда 2">Команда 2</option>
+                            <option value="Команда 3">Команда 3</option>
+                            <option value="Команда 4">Команда 4</option>
+                        </select>
+                    </div>
+
+                    <div className="inputgroup">
+                        <label> Выберите приоритет: </label>
+                        <select >
+                            <option value="Lowest">Lowest</option>
+                            <option value="Low">Low</option>
+                            <option value="Normal">Normal</option>
+                            <option value="Medium">Medium</option>
+                            <option value="High">High</option>
+                            <option value="Highest">Highest</option>
+                            <option value="Blocker">Blocker</option>
+                        </select>
+                    </div>
+
+                    <div className="inputgroup">
+                        <label> Дайте оценку задаче: </label>
+                        <input type="text" name="name" />
+                    </div>  
+                </div>
+                <div className="createclose">
+                    <p>
+                        <input type="submit" value="Добавить задачу" />
+                        <input type="submit" value="Закрыть" />
+                    </p>
+                </div>
+            </div>
+            )  
+        }
+   }
+
+export default CreateIssue;
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import LoginPage from './components/LoginPage';
+import CreateIssue from './components/CreateIssue';
 
 const Routes = (props) =>(
     <Switch>
         <Route path="/q" component={LoginPage}/>
+        <Route path="/create-issue" component={CreateIssue}/>
         {/*<Route path="*" component={NotFound}/>*/}
     </Switch>
 );

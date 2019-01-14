68d51c80a3edec6b67fa27aa44da5a8051cdf9b8
Dmitriyjd <dimon05091996abcd@mail.ru>
2019-01-14 12:13:27 +0300

    fix Issue styles add BoardPage view request to columns by board_id

--- src/containers/BoardPageView/BoardPageView.js
+++ src/containers/BoardPageView/BoardPageView.js
@@ -5,15 +5,11 @@ import BoardPageView from '../../components/BoardPageView'
 class BoardPageViewContainer extends PureComponent{
     constructor(props){
         super(props);
-        this.state = { data: [
-                {_id: "5c2e30e4fe742a43f0e08eb8",column_name: "TODO", board_id: "5c2e30e4fe742a43f0e08eb6"},
-                {_id: "5c2e30e4fe742a43f0e08eb9",column_name: "In progress",board_id:"5c2e30e4fe742a43f0e08eb6"},
-                {_id: "5c2e30e4fe742a43f0e08eba",column_name: "Code review",board_id:"5c2e30e4fe742a43f0e08eb6"},
-                {_id: "5c2e30e4fe742a43f0e08ebb",column_name: "Done",board_id:"5c2e30e4fe742a43f0e08eb6"}] };
+        this.state = { data: [] };
     }
 
     componentDidMount(){
-        /*this.makeColumnRequest()*/
+        this.makeColumnRequest()
     }
 
     makeColumnRequest(){
